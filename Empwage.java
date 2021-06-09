7a5ce65168b14d02963469990bf32a807bfc6da9
chaitra845 <chaitrachytra@gmail.com>
2021-06-09 23:38:02 +0530

    Added 20 working days in month

--- Empwage.java
+++ Empwage.java
@@ -3,13 +3,15 @@ public class Empwage
 	public static final int IS_PART_TIME = 1;
         public static final int IS_FULL_TIME = 2;
         public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int NUM_OF_WORKINGDAYS = 2;
 	
 	public static void main(String[] args) {
-
-	int empHrs = 0;
-	int empWage = 0;
+int empHrs = 0, empWage = 0, totalEmpWage = 0;
+for (int day = 0; day < NUM_OF_WORKINGDAYS; day++)
+{
 	int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-switch (empCheck ) {
+switch (empCheck) 
+{
 	case IS_PART_TIME:
 	   empHrs = 4;
 	   break;
@@ -17,9 +19,11 @@ switch (empCheck ) {
 	   empHrs = 8;
 	   break;
 	default:
-		empHrs = 0;
+	   empHrs = 0;
 }
 	empWage = empHrs * EMP_RATE_PER_HOUR;
-	System.out.println("Emp Wage: " + empWage);
+	totalEmpWage = empWage;
+}
+	System.out.println("Total Emp Wage: " + empWage);
 }
 }
