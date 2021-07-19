ff2204b1852868a792211de057e037637634579c
Chaitra.N.S <chaitrachytra@gmail.com>
2021-07-20 01:31:24 +0530

    Update Empwage.java

--- Empwage.java
+++ Empwage.java
@@ -3,7 +3,7 @@ public class Empwage
 	public static final int IS_PART_TIME = 1;
         public static final int IS_FULL_TIME = 2;
         public static final int EMP_RATE_PER_HOUR = 20;
-	public static final int NUM_OF_WORKING_DAYS = 2;
+	public static final int NUM_OF_WORKING_DAYS = 20;
         public static final int MAX_HRS_IN_MONTH = 10;
 	
 	public static void main(String[] args) {
